600cfc4a066b9ee532487a665ad5543da37352f8
Badal kumar singh <jpbadalsingh8899@gmail.com>
2022-07-22 22:23:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,97 +1,97 @@
-$(window).scroll(function(){
+$(window).scroll(function () {
     //====== scroll-up button show/hide script ======
-    if(this.scrollY > 80){
-      $('.scroll-up-btn').addClass("show");
+    if (this.scrollY > 80) {
+        $('.scroll-up-btn').addClass("show");
     }
-    else{
-      $('.scroll-up-btn').removeClass("show");
+    else {
+        $('.scroll-up-btn').removeClass("show");
     }
 });
-  
-$('.scroll-up-btn').click(function(){
-    $('html').animate({scrollTop: 0});
+
+$('.scroll-up-btn').click(function () {
+    $('html').animate({ scrollTop: 0 });
     //====== removing smooth scroll on slide-up button click ======
     $('html').css("scrollBehavior", "auto");
 });
-  
-  //====== Mobile Menu ======
-let nav = document.querySelector("nav"); 
+
+//====== Mobile Menu ======
+let nav = document.querySelector("nav");
 let body = document.querySelector("body");
 let navBar = document.querySelector(".navbar");
 let menuBtn = document.querySelector(".menu-btn");
 let cancelBtn = document.querySelector(".cancel-btn");
-  
-  
-  
-menuBtn.onclick = function (){
-  navBar.classList.add("active");
-  menuBtn.style.opacity ="0";
-  menuBtn.style.pointerEvents = "none";
-  body.style.overflow = "hidden";
+
+
+
+menuBtn.onclick = function () {
+    navBar.classList.add("active");
+    menuBtn.style.opacity = "0";
+    menuBtn.style.pointerEvents = "none";
+    body.style.overflow = "hidden";
 }
-  
-cancelBtn.onclick = function (){
-  navBar.classList.remove("active");
-  menuBtn.style.opacity ="1";
-  menuBtn.style.pointerEvents = "auto";
-  body.style.overflow = "auto";
-} 
-  
+
+cancelBtn.onclick = function () {
+    navBar.classList.remove("active");
+    menuBtn.style.opacity = "1";
+    menuBtn.style.pointerEvents = "auto";
+    body.style.overflow = "auto";
+}
+
 
 //====== Side Navigation Bar Close  ======
-  
+
 let navLinks = document.querySelectorAll(".menu li a");
-for(var i = 0; i < navLinks.length; i++){
-    navLinks[i].addEventListener("click", function(){
-      navBar.classList.remove("active");
-      menuBtn.style.opacity ="1";
-      menuBtn.style.pointerEvents = "auto";
-      body.style.overflow = "auto";
-  })
+for (var i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener("click", function () {
+        navBar.classList.remove("active");
+        menuBtn.style.opacity = "1";
+        menuBtn.style.pointerEvents = "auto";
+        body.style.overflow = "auto";
+    })
 }
-  
-  
-  //====== Scroll Fixed ======
-window.onscroll = function (){
-  if(document.documentElement.scrollTop > 20){
-      nav.classList.add("sticky");
-      logoOne.classList.add("active");
-      menuBtn.classList.add("active");
-  }
-  else{
-      nav.classList.remove("sticky");
-      logoOne.classList.remove("active");
-      menuBtn.classList.remove("active");
-  }
-  
-  //====== counter on scroll ======
-var number = 0;
-var oTop = $('#counter').offset().top - window.innerHeight;
-  if (number == 0 && $(window).scrollTop() > oTop) {
-    $('.counter-value').each(function () {
-      var $this = $(this),
-      countTo = $this.attr('data-count');
-      $({
-        countNum: $this.text()
-      }).animate({
-        countNum: countTo
-      },
-  
-      {
-  
-        duration: 2000,
-        easing: 'swing',
-        step: function () {
-          $this.text(Math.floor(this.countNum));
-        },
-        complete: function () {
-          $this.text(this.countNum); 
-        }
-     });
-   });
-  }
+
+
+//====== Scroll Fixed ======
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 20) {
+        nav.classList.add("sticky");
+        logoOne.classList.add("active");
+        menuBtn.classList.add("active");
+    }
+    else {
+        nav.classList.remove("sticky");
+        logoOne.classList.remove("active");
+        menuBtn.classList.remove("active");
+    }
+
+    //====== counter on scroll ======
+    var number = 0;
+    var oTop = $('#counter').offset().top - window.innerHeight;
+    if (number == 0 && $(window).scrollTop() > oTop) {
+        $('.counter-value').each(function () {
+            var $this = $(this),
+                countTo = $this.attr('data-count');
+            $({
+                countNum: $this.text()
+            }).animate({
+                countNum: countTo
+            },
+
+                {
+
+                    duration: 2000,
+                    easing: 'swing',
+                    step: function () {
+                        $this.text(Math.floor(this.countNum));
+                    },
+                    complete: function () {
+                        $this.text(this.countNum);
+                    }
+                });
+        });
+    }
 }
-  
+
 
 // ====== SEARCH FILTER ======
 const search = document.getElementById("search");
@@ -103,7 +103,7 @@ search.addEventListener("keyup", filterProducts);
 
 function filterProducts(e) {
     const text = e.target.value.toLowerCase();
-    productName.forEach(function(product) {
+    productName.forEach(function (product) {
         const item = product.firstChild.textContent;
         if (item.toLowerCase().indexOf(text) != -1) {
             product.parentElement.parentElement.style.display = "block"
@@ -127,9 +127,9 @@ allFilterBtns.forEach((btn) => {
     });
 });
 
-function showFilteredContent(btn){
+function showFilteredContent(btn) {
     allFilterItems.forEach((item) => {
-        if(item.classList.contains(btn.id)){
+        if (item.classList.contains(btn.id)) {
             resetActiveBtn();
             btn.classList.add('active-btn');
             item.style.display = "block";
@@ -139,7 +139,7 @@ function showFilteredContent(btn){
     });
 }
 
-function resetActiveBtn(){
+function resetActiveBtn() {
     allFilterBtns.forEach((btn) => {
         btn.classList.remove('active-btn');
     });
